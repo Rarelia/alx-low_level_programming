01a02cf7ca4b100cbac330ca21f735fb14431fdf
Rarelia <helixholt@gmail.com>
2022-09-16 14:03:04 -0700

    haha supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isupper - checks is a character is uppercase
+ * _isupper - checks is a character is uppercase or not
  * @c: carrier integer varaible
  * Return: 1 if true, 0 if false
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
