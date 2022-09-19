309f2da00f9b9d3759b3b5b39db58eb8c15313bd
Rarelia <helixholt@gmail.com>
2022-09-19 13:41:20 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,14 @@
 
 void puts_half(char *str)
 {
-	int x, y, i;
+	int i;
 
-	x = strlen(str);
-	if (x % 2 == 1)
-		y = x / 2 + 1;
-	else
-		y = x / 2;
-	for (i = y; i < x; i++)
+	for (i = 0; str[i] != '\0';i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
 		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
